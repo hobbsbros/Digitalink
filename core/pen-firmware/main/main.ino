7ba4682133e01c1c9f258da3ca7e13801d96f6ff
hobbsbros <89106819+hobbsbros@users.noreply.github.com>
2021-09-18 17:45:39 -0400

    Updated main.ino

---
+++ core/pen-firmware/main/main.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+
+const int MPU = 0x68;
+float AccX, AccY, AccZ;
+float roll, pitch, yaw;
+
+void setup () {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission(true);
+}
+
+void loop () {
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);
+  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0;
+  Serial.print(AccX + " | " + AccY + " | " + AccZ);
+}
